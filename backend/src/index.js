6472d971ad284822a72df64dc252954655ccf2f0
rohobie <ronitthakur267@gmail.com>
2026-01-24 12:17:29 +0530

    migrating to bun

--- backend/src/index.js
+++
@@ -1,7 +0,0 @@
-const RoomManager = require('./game/roomManager');
-
-const manager = new RoomManager();
-const roomId = manager.createRoom();
-console.log(`Room created: ${roomId}`);
-const game = manager.getRoom(roomId);
-console.log(game.getState());
